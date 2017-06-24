a16c1c96141243d6e1ca4c9468d996d4907cf198
Jose Ramirez <jsayram@Gmail.com>
2017-06-23 20:37:35 -0400

    Starting Template Update: got dynamic logo responding with the navbar,screen looks good across mutiple screeens

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 //used to open the side nav when the user presses the collapsed button
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "210px";
+    document.getElementById("main").style.marginLeft = "210px";
+    document.getElementById('name').style.display = 'none';
 }
 
 function closeNav() {
+    document.getElementById('name').style.display = 'block';
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+
+
 }
 
 $(function () {
