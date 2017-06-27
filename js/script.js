b576c284bf2d7354af65ee2a6c59898f0a9cd86a
Jose Ramirez <jsayram@Gmail.com>
2017-06-27 01:59:45 -0400

    fixed responsive layouts for different devices, speciallly for smaller screens, added a new image (subject to change), made the scrolling alot smoother when jumping to links inside the document

--- js/script.js
+++ js/script.js
@@ -1,18 +1,59 @@
 //used to open the side nav when the user presses the collapsed button
 function openNav() {
     document.getElementById("mySidenav").style.width = "210px";
-    document.getElementById("main").style.marginLeft = "210px";
+    document.getElementById("main").style.marginleft = "210px";
     document.getElementById('name').style.display = 'none';
+
+
+    if ($(window).width() > 360 && $(window).width() < 599) {
+        document.getElementById('logo').style.display = 'block';
+
+    } else if ($(window).width() > 599) {
+        document.getElementById('logo').style.display = 'block';
+        document.getElementById('name').style.display = 'block';
+    } else {
+        document.getElementById('logo').style.display = 'none';
+        document.getElementById('name').style.display = 'none';
+
+    }
+
 }
 
 function closeNav() {
-    document.getElementById('name').style.display = 'block';
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    if ($(window).width() < 360) {
+        document.getElementById('name').style.display = 'none';
+        document.getElementById('logo').style.display = 'block';
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
 
-
+    } else {
+        document.getElementById('name').style.display = 'block';
+        document.getElementById('logo').style.display = 'block';
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
 }
 
-$(function () {
+window.onorientationchange = function () {
+    location.reload()
+};
 
+
+
+//This allow scrolling to specified naviation to be smoother and
+// it doesnt jump past the clicked link, it gives a good amount of padding on the top
+
+jQuery(document).ready(function () {
+    //this prevents the default action
+    jQuery('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        var target = this.hash,
+            jQuerytarget = jQuery(target);
+
+        jQuery('html,body').stop().animate({
+            'scrollTop': jQuerytarget.offset().top - 90
+        }, 700, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });
